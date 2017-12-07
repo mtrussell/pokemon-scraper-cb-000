751fa2d4b1dce56d203d603fa181f5e8552d465d
Matt Trussell <smtrussell@gmail.com>
2017-12-07 20:14:53 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,10 @@
 require "pry"
 
 class Pokemon
-  attr_reader :id, :name, :type, hp:, :db
+  attr_reader :id, :name, :type, :hp, :db
   @@all = []
 
-  def initialize (id:, name:, type:, hp: = null, db:)
+  def initialize (id:, name:, type:, hp: nil, db:)
     @id = id
     @name = name
     @type = type
